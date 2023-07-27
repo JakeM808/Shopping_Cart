d42a1ba75e3a01084e4898aa3f53adf40a7af3e7
Jake Matthews <jake.m86@gmail.com>
2023-07-26 14:37:20 -1000

    various error and bug fixes

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,37 +2,44 @@
 cart = {}
 
 # Function to add items
-def add_item(item, quantity, price):
+def add_item(item, quantity=1, price=0):
     # If new item, add to cart
     if item not in cart:
-        cart[item] = quantity
+        cart[item] = (quantity, price)
+        
+
+    
     # Else, increase the quantity of existing item
     else:
-        cart[item] += quantity
+        cart[item] = (cart[item][0] + quantity, cart[item][1])
 
 # Function to remove items
-def remove_item(item):
+def remove_item(item, quantity=1):
     # Remove items by user input
     if item in cart:
-        cart[item] -= quantity
-    # If quantity of item <= 0, delete item
-    if cart[item] <= 0:
-        del cart[item]
+        cart[item] = (cart[item][0] - quantity, cart[item][1])
+    else:
+        print('There are no cart[item]"s" in your cart')
 
 # Function to display items and values
 def view_cart():
+    total = 0
     print("Items in your cart:")
-    for item, quantity in cart.items():
-        print(f"{item}: {quantity} x ${item.price}")
+    for item, tup in cart.items():
+        item_total = round(tup[0] * tup[1],2)
+        
+        print(f"You have {tup[1]} {item} in your cart, which cost ${item_total})")
+        total += item_total
+        print(f"Total: ${total}")
 
 
 # Function to display receipt and exit
 def quit():
     print("Receipt:")
     total = 0 
-    for item, quantity in cart.items():
-        print(f"{item}: {quantity} x ${item.price}")
-        total += quantity * item.price
+    for item, (quantity, price) in cart.items():
+        print(f"{item}: {quantity} x ${price}")
+        total += round(quantity * price,2)
     print(f"Total: ${total}")
 
     cart.clear()
@@ -51,7 +58,7 @@ while True:
     print("Quit")
 
     # Get user input
-    choice = input("What would you like to do? ")
+    choice = input("What would you like to do? ").title()
 
     # User selction add to cart
     if choice == 'Add':
@@ -63,9 +70,9 @@ while True:
     # User selection remove from cart
     elif choice == 'Remove':
         item = input("Enter the item: ")
-        quantity = input('How many would you like to remove? ')
-        remove_item(item)
-    # Vew
+        quantity = int(input('How many would you like to remove? '))
+        remove_item(item, quantity)
+    # View
     elif choice == 'View':
         view_cart()
     # Quit
